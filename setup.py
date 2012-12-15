9f8d54a32d91cb5c0fcb3a2941b58825db3d9756
draftcode <draftcode@gmail.com>
2012-12-15 19:40:46 +0900

    Fix a design issue. Add setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim: fileencoding=utf-8
+from setuptools import setup
+
+setup(
+    name='draftcode.github.com',
+    description='Source of draftcode.github.com',
+    author='draftcode',
+    author_email='draftcode@gmail.com',
+    url='http://draftcode.github.com',
+    packages=[],
+    install_requires=['pelican', 'ghp-import'],
+    )
+
