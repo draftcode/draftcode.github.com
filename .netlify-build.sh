67176658a62112884d62544c480c1530789a02f0
Masaya Suzuki <masayasuzuki@google.com>
2021-01-24 14:39:06 -0800

    Add a build script

---
+++ .netlify-build.sh
@@ -0,0 +1,5 @@
+curl -LO "https://github.com/bazelbuild/bazelisk/releases/download/v1.1.0/bazelisk-linux-amd64"
+mkdir -p bin
+mv bazelisk-linux-amd64 bin/bazel
+chmod +x bin/bazel
+bin/bazel build //...
