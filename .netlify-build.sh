d1a4ed7c85283ebaa31f8e2f1f1f6d7ab5f289dc
Masaya Suzuki <masayasuzuki@google.com>
2021-01-24 20:04:15 -0800

    Fix the bazel command

--- .netlify-build.sh
+++ .netlify-build.sh
@@ -7,4 +7,4 @@ if [ ! -e "$BAZEL" ]; then
   (cd "$NETLIFY_CACHE_DIR" && curl -LO "https://github.com/bazelbuild/bazelisk/releases/download/v1.1.0/bazelisk-linux-amd64")
   chmod +x "$BAZEL"
 fi
-"$BAZEL"--batch build --disk_cache="${NETLIFY_CACHE_DIR}/bazel" //draftcode.osak.jp:final_layout
+"$BAZEL" --batch build --disk_cache="${NETLIFY_CACHE_DIR}/bazel" //draftcode.osak.jp:final_layout
