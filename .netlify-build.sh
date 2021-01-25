81589581dc642967a6f2c011db524019b0b124a8
Masaya Suzuki <masayasuzuki@google.com>
2021-01-24 20:01:29 -0800

    Use cache

--- .netlify-build.sh
+++ .netlify-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-curl -LO "https://github.com/bazelbuild/bazelisk/releases/download/v1.1.0/bazelisk-linux-amd64"
-mkdir -p bin
-mv bazelisk-linux-amd64 bin/bazel
-chmod +x bin/bazel
-bin/bazel --batch build  //draftcode.osak.jp:final_layout
+NETLIFY_CACHE_DIR=/opt/buildhome/cache
+BAZEL="${NETLIFY_CACHE_DIR}/bazelisk-linux-amd64"
+
+if [ ! -e "$BAZEL" ]; then
+  (cd "$NETLIFY_CACHE_DIR" && curl -LO "https://github.com/bazelbuild/bazelisk/releases/download/v1.1.0/bazelisk-linux-amd64")
+  chmod +x "$BAZEL"
+fi
+"$BAZEL"--batch build --disk_cache="${NETLIFY_CACHE_DIR}/bazel" //draftcode.osak.jp:final_layout
